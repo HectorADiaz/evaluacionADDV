ff4fbbb05364c94cceedd0e2be578a3799b42419
Hector <loginhectordf@gmail.com>
2023-10-16 19:46:22 -0600

    insert register into table characters and create query for count characters

--- db/SeccionSQL.sql
+++ db/SeccionSQL.sql
@@ -1,8 +1,31 @@
 -- Creación de la base de datos
-create database evaluacion 
-use evaluacion
+CREATE DATABASE evaluacion 
+USE evaluacion
 
 -- Creación de la tabla
-create table characters (
+CREATE TABLE characters (
 charName NVARCHAR(100)
 ) 
+
+-- Insercion de registro
+INSERT INTO characters VALUES('hello world') 
+SELECT * FROM characters
+
+-- Query
+SELECT letra, COUNT(letra) AS cantidad
+FROM (
+  SELECT SUBSTRING(charName, number, 1) AS letra
+  FROM characters 
+  CROSS JOIN (
+    SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS number
+    FROM master..spt_values
+  ) AS numbers
+  WHERE number <= LEN(charName) AND SUBSTRING(charName, number, 1) <> ' '
+) AS letras
+GROUP BY letra
+ORDER BY letra; 
+ 
+
+
+
+
